2a47adbdc057a7bb75c921f0f03f84c3960e33ba
LukaszNowakowski <Lukasz.Nowakowski@axadirect-solutions.pl>
2016-05-31 14:24:28 +0200

    Remove unnecessary files.

--- ChangeVersionAxa.ps1
+++
@@ -1,5 +0,0 @@
-.\ChangeVersion.ps1 `
-	-GitRepositoryRoot git@github.com:Operasoft/ `
-	-RootDirectory C:/TelematicsWorkspaces/changeVersion `
-	-NewVersion 1.38.0.0 `
-	-BranchName branch-automation-refactoring
